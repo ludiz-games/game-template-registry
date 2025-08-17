b116c435024ddca583d17bddcad6908376f391d5
jide <jide.fr@gmail.com>
2025-08-17 23:58:40 +0200

    better auth

--- packages/backend/convex/http.ts
+++
@@ -1,9 +0,0 @@
-import { httpRouter } from "convex/server";
-import { auth } from "../better-auth/server";
-import { betterAuthComponent } from "./auth";
-
-const http = httpRouter();
-
-betterAuthComponent.registerRoutes(http, auth);
-
-export default http;
